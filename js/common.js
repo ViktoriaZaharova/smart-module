add63184087ccb2904d3e07636ecf684d4d969cb
Viktoria <zaharova181114@gmail.com>
2023-01-20 20:39:07 +0200

    add index3

--- js/common.js
+++ js/common.js
@@ -18,3 +18,12 @@ $('.product-card-slider').slick({
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
 });
 
+// mobile menu
+$('.btn-burger').on('click', function () {
+	$('.mobile-menu').fadeToggle();
+});
+
+$('.mobile-menu__close').on('click', function () {
+	$('.mobile-menu').fadeOut();
+});
+
